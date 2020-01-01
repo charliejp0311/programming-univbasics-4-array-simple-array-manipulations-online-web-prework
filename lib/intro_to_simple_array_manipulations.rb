0ecd2633cc5bb89c9904c3210b7ff6bc12b3def5
Charles Pelton <charliejp0311@gmail.com>
2020-01-01 01:29:12 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,20 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+#using_unshift(bouroughs_in_nyc, @new_neighborhood)
+
+def using_pop(array)
+  array.pop 
+end
+
+using_pop(@continents)
+
+def pop_with_args(array)
+  array.pop 
+end
+
